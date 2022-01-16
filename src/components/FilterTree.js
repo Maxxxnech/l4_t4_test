bf6f82e9815474a2a6721224ffe0167a9b98b73b
Maxxxnech <51296105+Maxxxnech@users.noreply.github.com>
2022-01-16 18:16:13 +0300

    deep clone at filter

--- src/components/FilterTree.js
+++ src/components/FilterTree.js
@@ -3,7 +3,6 @@ import Tree from "./Tree";
 import "./css/FilterTree.css";
 
 export default class FilterTree extends Component {
-
   constructor(props) {
     super(props);
     this.state = { cars: this.props.cars };
@@ -11,41 +10,45 @@ export default class FilterTree extends Component {
     this.filterTree = this.filterTree.bind(this);
   }
 
-
   filterTree(tree, value) {
     const valueLowercase = value.toLowerCase();
-    return tree?.filter((el)=> {
-       if(!el) return false;
-      
-       if(el.models){
+
+    let newTree = JSON.parse(JSON.stringify(tree)); // Needed for proper updates at shortening text
+
+    return newTree?.filter((el) => {
+      if (!el) return false;
+
+      if (el.models) {
         el.models = this.filterTree(el.models, value);
-       } 
-       if((el.models && el.models.length) || el.title.toLowerCase().includes(valueLowercase)) return true;
+      }
+      if (
+        (el.models && el.models.length) ||
+        el.title.toLowerCase().includes(valueLowercase)
+      )
+        return true;
     });
   }
 
   handleChange(e) {
-    const currentValue = e.target.value;
-    console.log('currentValue: ' + currentValue)
-    if(!currentValue){
-        console.log('' + this.props.cars.length);
-        return this.setState((prevState) => ({
-            cars: this.props.cars,
-          }));
+    let currentValue = e.target.value;
+
+    if (!currentValue) {
+      return this.setState({
+        cars: this.props.cars,
+      });
     }
     let newCars = this.filterTree(this.props.cars, currentValue);
-    console.log(newCars);
+    console.log(this.props.cars.length, newCars.length);
     this.setState((prevState) => ({
       cars: newCars,
     }));
   }
 
   render() {
-    const {cars} = this.state;
-    //console.log(cars);
+    const { cars } = this.state;
     return (
       <div>
-        <input ref={this.inputRef} onChange={this.handleChange} type="text" />
+        <input ref={this.inputRef} onChange={this.handleChange} type="text" placeholder="Car name" />
         {cars && cars.length ? (
           <Tree cars={cars} />
         ) : (
